be0df370d471ddf3f0ca690c62bf59e854f9f475
stealthmate <stealthmate1233@gmail.com>
2019-11-29 02:14:18 +0900

    Fix single dollar math mode to double dollar when converting from jupyter to markdown

--- excluded/trunclines.py
+++ excluded/trunclines.py
@@ -1,10 +1,13 @@
 import sys
 import re
 
+def fixLatex(line):
+    return re.sub(r"(?<!\$)\$(.+?)\$(?!\$)", r"\n$$\1$$\n", line)
+
 if __name__ == '__main__':
     fi = sys.argv[1]
     lines = []
     with open(fi) as f:
-        lines = ''.join(f.readlines())
+        lines = ''.join(fixLatex(line) for line in f.readlines())
     with open(fi, mode='w') as f:
         f.write(re.sub(r'\n\n\n+', '\n\n', lines))
